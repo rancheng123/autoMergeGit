8db3c7e9fd7f7a9e43b9b0fc8bc983b6b8dae992
rancheng <rancheng@jwzg.com>
2021-08-09 10:34:03 +0800

    11

--- projects/lanhu/merge_lanhu_dev.js
+++ projects/lanhu/merge_lanhu_dev.js
@@ -6,5 +6,9 @@ merge({
     //目标分支
     target_branch: 'develop',
     //工作目录
-    workPlace: '/Users/apple/workPlace/components'
+    workPlace: '/Users/apple/workPlace/components',
+    // tag: {
+    //     version: 'V3.5',
+    //     msg: 'release 3.5'
+    // }
 })
